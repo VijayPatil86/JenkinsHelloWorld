e6197f3d509fd9b22dd40c1cb67e9c253dd7afc4
Vijay Patil <vijipatil090386@gmail.com>
2022-09-13 12:16:37 +0530

    change done

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 class HelloWorld {
 	public static void main(String[] args){
-<<<<<<< HEAD
-		System.out.println("Hello World...1113");
-=======
 		System.out.println("Hello World...200007m6");
->>>>>>> 92998c95842b4a2300b806454cb851021b0feb2d
 	}
 }
